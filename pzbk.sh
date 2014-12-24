67fa8336c683a9e9f113f6eba9279b4a74960933
lifesfun <dejong.nk@gmail.com>
2014-12-23 20:34:02 -0500

    working fix

--- pzbk.sh
+++ pzbk.sh
@@ -2,23 +2,27 @@
 ##### Backup Functions #####
 
 function pzBkConf(){
+    pzBkPath="/var/backups/pzbackup"
     [ -d "$pzBkPath" ] || mkdir "$pzBkPath" 
     pzAzPath="$pzBkPath"/archive 
     [ -d "$pzAzPath" ] || mkdir "$pzAzPath" 
 }
 function pzBk(){
-    pzBkConf
+	pzBkConf
 	pzGetNow "pzBackup: Saving the past..." 
 	local bkName="$pzNow"_bk.tar
 	pzCmd save
-    ionice -c3 tar -C "$pzAzPath" -cf "$bkName" "$pzMpPath" "$pzZPath"/Server "$pzZPath"/db 
+	cd "$pzAzPath" 
+	tar -C "$pzAzPath" -cpf "$bkName" "$pzMpPath" "$pzZPath"/Server "$pzZPath"/db 
 }
 function pzAz(){
     pzBkConf
     local day=$(date +%y.%m.%d )
     local archive="$pzAzPath"/"$day"_pzAz.tar
     local latest="$(find $pzAzPath -type f -name "$day*_bk.tar" | tail -1 )" || exit
-    mv "$latest" "$archive" &&  rm -rf "$day"T*
-    gzip "$archive" 
-    #sftp "$bk_srv" -C put "$archive"  
+    mv "$latest" "$archive" &&  rm -rf "$day"*T*
+	if [ -f "$archive" ] ; then 
+		[ -f "$archive"'.gz' ] && rm "$archive"'.gz'  
+		gzip "$archive" 
+	fi
 }
